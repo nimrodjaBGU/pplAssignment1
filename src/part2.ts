ea06c9d0a7df6bd1895e3ce4d8ec20b36f23795f
Nimrod Jakobi <nimrodja@post.bgu.ac.il>
2026-04-05 02:07:03 +0300

    done part 2

--- src/part2.ts
+++ src/part2.ts
@@ -1,13 +1,27 @@
 import * as R from "ramda";
+import {pipe} from "ramda";
 
 const stringToArray = R.split("");
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: (s: string) => number = undefined as any;;
+export const countVowels = (s: string): number =>
+  pipe(
+    stringToArray,
+    (chars: string[]) => chars.filter(c => "aeiouAEIOU".includes(c)),
+    (vowels: string[]) => vowels.length
+  )(s);
 
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
+export const isPalindrome = (text: string): boolean => {
+    const cleaned = pipe(stringToArray,(char: string[]) => char.filter(c => /[a-zA-Z0-9]/.test(c)),(letters: string[]) => letters.map(l => l.toLowerCase))(text);
+
+    const reversed = cleaned.reduce((acc, cur) => cur + acc, "")
+
+    return cleaned.join("") ===reversed;
+}
+    
+
   
 
 /* Question 2.3 */
@@ -16,4 +30,8 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+export const treeToSentence = (t: WordTree): string => {
+    if(t.children.length===0) return t.root;
+    const childrenSentence = t.children.map(child => treeToSentence(child)).reduce((acc , cur) => acc + " " + cur , "");
+    return t.root + " " + childrenSentence;
+}
